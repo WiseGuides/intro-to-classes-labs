bed5c23a56bc9b2ac1d9cf0cf4464e388160aa98
Hal Helms <hal.helms@gmail.com>
2019-01-22 15:33:38 -0800

    10 exercises

---
+++ index.js
@@ -0,0 +1,42 @@
+// Intro to Classes Lab
+// Rev. 22Jan2019
+
+/*
+1. Create a class, Car, with a constructor that accepts make, model, color, and year and stores those.
+*/
+
+/*
+2. Create an array, cars, and populate it with 5 different car objects
+*/
+
+/*
+3. Create a Product class with a constructor that takes name, and price and stores those.
+*/
+
+/*
+4. Create a LineItem class with a constructor that takes a product object and a quantity and stores those.
+*/
+
+/*
+5. Create an Address class that accepts/stores street, city, state, and zip.
+*/
+
+/*
+6. Create a User class with a constructor that accepts name and address.
+*/
+
+/*
+7. Create a ShoppingCart class with a constructor that accepts/stores a User and creates/stores a lineItems array. Give it an addProduct method that accepts a Product and a quantity. The addProduct method should check to see if there is already a LineItem object with that product in it. If so, it should increment the quantity. If not, it should create a LineItem object.
+*/
+
+/*
+8. Instantiate a ShoppingCart with a constructor that accepts a user.
+*/
+
+/*
+9. Experiment with the shoppingCart object.
+*/
+
+/*
+10. Create a RaceCar class that inherits from Car. Its constructor should accept make, model, color, and year -- and call super, sending those arguments. It should also have a race() method that outputs 'Racing'
+*/
